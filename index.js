4b0d4aaa16a95906483f1bab3f2f0516b9f716d9
Mike Nolan <michael.nolan@parivedasolutions.com>
2017-08-04 20:13:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mikenolan on 8/4/17.
+ */
+let express = require('express');
+let app = express();
+
+app.get('/index', function(request, response) {
+   response.json({"hello": "world"});
+});
+
+app.listen(3001);
